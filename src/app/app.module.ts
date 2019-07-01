661bb1cf9575eec2d49808ce2f0a41d5d76d8499
Lu Tuan Dat <drianliz0411@gmail.com>
2019-07-01 16:15:37 +0700

    editing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
-    NavBarComponent
+    NavBarComponent,
+    SideBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
